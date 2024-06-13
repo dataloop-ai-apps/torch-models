948eff98cdd52ccf2f2b4dbf41155227792dc260
RoniDataloopAi <roni.a@dataloop.ai>
2024-06-13 17:21:08 +0300

    clean branch before merge

--- adapters/deeplabv3/test-adapter.py
+++ adapters/deeplabv3/test-adapter.py
@@ -3,18 +3,18 @@ import dtlpy as dl
 
 if __name__ == '__main__':
     # Predict with pre-trained weights
-    # project = dl.projects.get(project_name="")
-    # dataset = project.datasets.get(dataset_name="")
-    # model = project.models.get(model_id="")
-    # item = dataset.items.get(item_id="")
-    # adapter = ModelAdapter(model)
-    # adapter.predict_items([item])
+    project = dl.projects.get(project_name="")
+    dataset = project.datasets.get(dataset_name="")
+    model = project.models.get(model_id="")
+    item = dataset.items.get(item_id="")
+    adapter = ModelAdapter(model)
+    adapter.predict_items([item])
     #
     # # # Train
-    # project = dl.projects.get(project_name="")
-    # model = project.models.get(model_id="")
-    # adapter = ModelAdapter(model)
-    # adapter.train_model(model)
+    project = dl.projects.get(project_name="")
+    model = project.models.get(model_id="")
+    adapter = ModelAdapter(model)
+    adapter.train_model(model)
 
     # Predict with new weights
     project = dl.projects.get(project_name="")
